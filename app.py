00723c6d9eb3bac8f6058c619c1913e9d51e938d
Kevin <kevin.netherton@gov.bc.ca>
2016-06-02 14:16:54 -0700

    try now!

--- app.py
+++ app.py
@@ -21,4 +21,5 @@ def getLiquorStoreDemo():
     return jsonify(result={"status": 200})
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    #app.run(host='0.0.0.0', port=8080)
+    pass

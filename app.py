661495045cfb4afe138527cc875164f2b5f4f3db
Kevin <kevin.netherton@gov.bc.ca>
2016-06-02 13:35:39 -0700

    adding requirements

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ import LiquorLocator
 def hello():
     return "Hello World!"
 
-#def getLiquorStoreDemo():
-#    demoPoints = [-126.844567, 49.97859, -122.799997, 58.925305]'
-#    LiquorLocator.RouteLiquor()
+def getLiquorStoreDemo():
+    demoPoints = [-126.844567, 49.97859, -122.799997, 58.925305]
+    LiquorLocator.RouteLiquor(demoPoints)
+    
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)

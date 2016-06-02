d01602b1cfad975ddc42d00e00cf3a6975fbb466
Leo <leo.lou@gov.bc.ca>
2016-06-02 13:48:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ site.addsitedir(dir2Add)
 import LiquorLocator
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+from flask import Flask, request
+# set the project root directory as the static folder, you can set others.
+app = Flask(__name__, static_url_path='')
+
+@app.route('/')
+def root():
+    return app.send_static_file('osm-map-demo.html')
 
 def getLiquorStoreDemo():
     demoPoints = [-126.844567, 49.97859, -122.799997, 58.925305]

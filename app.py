1d3be32b13cbaf4571096843a308e2c1a6fa28ed
Leo <leo.lou@gov.bc.ca>
2016-06-02 13:51:46 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
+from flask import Flask, request
 import site
 dir2Add = './src'
 site.addsitedir(dir2Add)
 import LiquorLocator
-
-
-from flask import Flask, request
 # set the project root directory as the static folder, you can set others.
 app = Flask(__name__, static_url_path='')
 
